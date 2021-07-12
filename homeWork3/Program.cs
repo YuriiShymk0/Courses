3b27aaf9b5d6dbeac0789929042d10337b705460
Юрій Шимко <jushymko@kpi.ua>
2021-07-12 18:50:41 +0300

    homework 3

--- homeWork3/Program.cs
+++ homeWork3/Program.cs
@@ -6,9 +6,78 @@ namespace homeWork3
     {
         static void Main()
         {
-            int a = 1;
-            int b = 2;
-            Console.WriteLine("Resu");
+            bool continueGame = true;
+            int currentHp = 100;
+            while (continueGame)
+            {
+                Console.Clear();
+                Console.WriteLine(string.Format("Your current hp: {0}", currentHp));
+                Console.Write("Enter number or write \"EXIT\" to end game: ");
+                string choosen = Console.ReadLine();
+                if (!int.TryParse(choosen, out int choosenNumber))
+                {
+                    if (choosen.ToUpper() == "EXIT")
+                    {
+                        Console.WriteLine("Exit from game...");
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect number");
+                        Console.WriteLine("Press Enter to continue");
+                        Console.ReadLine();
+                        continue;
+                    }
+                }
+                if (choosenNumber == 0)
+                {
+                    Console.WriteLine("Choose not 0! Choose any another number");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                int rnd = new Random().Next(0, 5);
+                switch (rnd % choosenNumber)
+                {
+                    case 0:
+                        Console.WriteLine("You win");
+                        continueGame = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("-10hp");
+                        currentHp -= 10;
+                        break;
+                    case 2:
+                        Console.WriteLine("+10hp");
+                        currentHp += 10;
+                        break;
+                    case 3:
+                        Console.WriteLine("-5hp");
+                        currentHp -= 5;
+                        break;
+                    case 4:
+                        Console.WriteLine("+5hp");
+                        currentHp += 5;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect number");
+                        break;
+                }
+                if (currentHp > 0)
+                {
+                    Console.WriteLine(string.Format("Your current hp: {0}", currentHp));
+                }
+                else
+                {
+                    Console.WriteLine("Game over");
+                    continueGame = false;
+                }
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+            }
         }
     }
 }
