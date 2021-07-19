434b9775241843999a602692bd211a58406a93d0
Юрій Шимко <jushymko@kpi.ua>
2021-07-19 19:20:27 +0300

    add  homeWork 6

---
+++ homeWork5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace homeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 2, 2 };
+            Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ homeWork6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace homeWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 1, 2, 2, 2 };
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
